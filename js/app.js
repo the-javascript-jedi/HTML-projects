7d2a9d018e7d4125d12cc2736fe1d060b23b5ae6
Nithin Samuel <nithin.thewebdev@gmail.com>
2025-01-30 15:17:22 +0530

    Removed tracked files now in .gitignore

--- js/app.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("cart-info").addEventListener("click", function() {
-  const cart = document.getElementById("cart");
-  cart.classList.toggle("show-cart");
-  console.log(cart);
-});
